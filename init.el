0e5c58963d32f036dc7e3eb358b24870596ca149
Abhiroop Datta <abdatta@ixiacom.com>
2019-02-25 20:05:06 +0530

    adding support for smart-mode-line

--- init.el
+++ init.el
@@ -22,4 +22,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(c-comment-only-line-offset 0)
+ '(c-default-style (quote ((c++-mode . "ellemtel") (c-mode . "linux"))))
+ '(c-hanging-braces-alist
+   (quote
+    ((defun-open before after)
+     (defun-close before after)
+     (class-open before after)
+     (block-close . c-snug-do-while)
+     (statement-cont)
+     (substatement-open after)
+     (brace-list-open)
+     (brace-entry-open)
+     (extern-lang-open after)
+     (namespace-open)
+     (namespace-close)
+     (module-open after)
+     (composition-open after)
+     (inexpr-class-open after)
+     (inexpr-class-close before)
+     (arglist-cont-nonempty))))
+ '(c-indent-comments-syntactically-p t)
+ '(c-offsets-alist
+   (quote
+    ((defun-block-intro . +)
+     (func-decl-cont . +)
+     (knr-argdecl-intro . +)
+     (topmost-intro . 0))))
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(tab-width 4))
